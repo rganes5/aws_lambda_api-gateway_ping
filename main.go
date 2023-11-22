e07694a38e5a98d2b4834f816d1820569e47f51b
Ganesh <ganeshraveendranit@gmail.com>
2023-11-22 09:55:58 +0530

    final commit

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	response := events.APIGatewayProxyResponse{
 		StatusCode: 200,
+		Body:       "Hello Ganesh !",
 	}
 
 	return response, nil
